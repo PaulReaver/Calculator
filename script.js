23e67519fba41d2a51bc537f7fa298a30130a645
Paul Vallas <35564306+PaulReaver@users.noreply.github.com>
2022-12-19 14:21:55 +0200

    Formatted code

--- script.js
+++ script.js
@@ -1,135 +1,132 @@
-//Initializes array with numbers and operator
-let array = ["", null, ""];
+// Initializes array with numbers and operator
+const array = ['', null, ''];
 
-//Displays 0 at the beginning
-let displayResult = document.querySelector("#display-result");
-displayResult.textContent = array[0];
+// Displays 0 at the beginning
+const displayResult = document.querySelector('#display-result');
+[displayResult.textContent] = array;
 
-//Resets the calculator values when the user presses the clear button
-document.querySelector("#clear").addEventListener("click", () => {
-    array[0] = "";
+// Resets the calculator values when the user presses the clear button
+document.querySelector('#clear').addEventListener('click', () => {
+    array[0] = '';
     array[1] = null;
-    array[2] = "";
-    displayResult.textContent = array[0];
+    array[2] = '';
+    [displayResult.textContent] = array;
 });
 
-//Deletes a digit when the user presses the backspace button
-document.querySelector("#backspace").addEventListener("click", () => {
+// Deletes a digit when the user presses the backspace button
+document.querySelector('#backspace').addEventListener('click', () => {
     if (array[1] == null) {
         array[0] = array[0].slice(0, -1);
-        displayResult.textContent = array[0];
+        [displayResult.textContent] = array;
     } else {
         array[2] = array[2].slice(0, -1);
         displayResult.textContent = `${array[0]} ${array[1]} ${array[2]}`;
     }
-})
+});
 
-//Adds a decimal point when the user presses the decimal button
-document.querySelector("#decimal").addEventListener("click", () => {
+// Adds a decimal point when the user presses the decimal button
+document.querySelector('#decimal').addEventListener('click', () => {
     if (array[1] == null) {
-        if (array[0].includes(".") == false) {
-            array[0] = array[0].concat(".");
-            displayResult.textContent = array[0];
+        if (array[0].includes('.') === false) {
+            array[0] = array[0].concat('.');
+            [displayResult.textContent] = array;
         }
-
     } else if (array[1] != null) {
-        if (array[2].includes(".") == false) {
-            array[2] = array[2].concat(".");
+        if (array[2].includes('.') === false) {
+            array[2] = array[2].concat('.');
             displayResult.textContent = `${array[0]} ${array[1]} ${array[2]}`;
         }
     }
-})
+});
 
-//Operates when the user presses the equals button
-document.querySelector("#equals").addEventListener("click", () => {
-    //Checks if there is an operator already selected so it can skip the function
-    if (array[1] == null) {
-        return;
-    }
-    //Operates and resets values of the array
-    array[2] = +array[2];
-    array[0] = operate(array[0], array[1], array[2]);
-    array[0] = String(array[0]);
-    array[1] = null;
-    array[2] = "";
-    displayResult.textContent = array[0];
-    //Resets the first number to 0 if there is an error
-    if (array[0] == "error") {
-        array[0] = "";
-    }
-})
-
-//Gets user number depending whether there is an operator selected
-for (let i = 0; i <= 9; i++) {
-    document.querySelector(`#num${i}`).addEventListener("click", () => {
-        if (array[1] == null) {
-            array[0] = array[0].concat(i);
-            displayResult.textContent = array[0];
-        } else {
-            array[2] = array[2].concat(i);
-            displayResult.textContent = `${array[0]} ${array[1]} ${array[2]}`;
-        }
-    })
-}
-
-//Gets an operator
-for (let i = 1; i <= 4; i++) {
-    document.querySelector(`#op${i}`).addEventListener("click", () => {
-        if (array[1] != null) {
-            array[2] = +array[2]; 
-            array[0] = operate(array[0], array[1], array[2]);
-            array[2] = "";
-        }
-        array[0] = +array[0];
-        if (`#op${i}` == "#op1") {
-            array[1] = "+";
-        } else if (`#op${i}` == "#op2") {
-            array[1] = "-";
-        } else if (`#op${i}` == "#op3") {
-            array[1] = "*";
-        } else {
-            array[1] = "/";
-        }
-        displayResult.textContent = `${array[0]} ${array[1]}`;
-    })
-}
-
-//Function that perform addition
+// Function that perform addition
 function add(a, b) {
     return a + b;
 }
 
-//Function that performs subtraction
+// Function that performs subtraction
 function subtract(a, b) {
     return a - b;
 }
 
-//Function that performs multiplication
+// Function that performs multiplication
 function multiply(a, b) {
     return a * b;
 }
 
-//Function that performs division
+// Function that performs division
 function divide(a, b) {
-    if (b == 0) {
-        return "error";
+    if (b === 0) {
+        return 'error';
     }
     return a / b;
 }
 
-//Function responsible for calling the right function to perform an operation
+// Function responsible for calling the right function to perform an operation
 function operate(x, operator, y) {
     let result;
-    if (operator == "+") {
+    if (operator === '+') {
         result = add(x, y);
-    } else if (operator == "-") {
+    } else if (operator === '-') {
         result = subtract(x, y);
-    } else if (operator == "*") {
+    } else if (operator === '*') {
         result = multiply(x, y);
-    } else if (operator == "/") {
+    } else if (operator === '/') {
         result = divide(x, y);
     }
     return result;
 }
 
+// Operates when the user presses the equals button
+document.querySelector('#equals').addEventListener('click', () => {
+    // Checks if there is an operator already selected so it can skip the function
+    if (array[1] == null) {
+        return;
+    }
+    // Operates and resets values of the array
+    array[2] = +array[2];
+    array[0] = operate(array[0], array[1], array[2]);
+    array[0] = String(array[0]);
+    array[1] = null;
+    array[2] = '';
+    [displayResult.textContent] = array;
+    // Resets the first number to 0 if there is an error
+    if (array[0] === 'error') {
+        array[0] = '';
+    }
+});
 
+// Gets user number depending whether there is an operator selected
+for (let i = 0; i <= 9; i += 1) {
+    document.querySelector(`#num${i}`).addEventListener('click', () => {
+        if (array[1] == null) {
+            array[0] = array[0].concat(i);
+            [displayResult.textContent] = array;
+        } else {
+            array[2] = array[2].concat(i);
+            displayResult.textContent = `${array[0]} ${array[1]} ${array[2]}`;
+        }
+    });
+}
+
+// Gets an operator
+for (let i = 1; i <= 4; i += 1) {
+    document.querySelector(`#op${i}`).addEventListener('click', () => {
+        if (array[1] != null) {
+            array[2] = +array[2];
+            array[0] = operate(array[0], array[1], array[2]);
+            array[2] = '';
+        }
+        array[0] = +array[0];
+        if (`#op${i}` === '#op1') {
+            array[1] = '+';
+        } else if (`#op${i}` === '#op2') {
+            array[1] = '-';
+        } else if (`#op${i}` === '#op3') {
+            array[1] = '*';
+        } else {
+            array[1] = '/';
+        }
+        displayResult.textContent = `${array[0]} ${array[1]}`;
+    });
+}
